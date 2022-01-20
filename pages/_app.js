7985421424e7db682d7e084edd761b589705406d
giacomogaglione <gaglione.giacomo@gmail.com>
2022-01-20 23:21:23 +0100

    main

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ function MyApp({ Component, pageProps }) {
 export default MyApp
 
 */
+import Header from "../components/Header";
+import Footer from "@components/Footer";
+
 import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -26,13 +29,15 @@ export default function MyApp(props) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>MUI5 Nextjs</title>
+        <title>Wine Compare</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <Header />
         <Component {...pageProps} />
+        <Footer />
       </ThemeProvider>
     </CacheProvider>
   );
